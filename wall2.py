e61d1d93b51ec7cc1da68a2837042779ff025ae6
H0nk3RMan <127336752+H0nk3RMan@users.noreply.github.com>
2023-03-08 14:34:52 -0500

    Add files via upload

---
+++ wall2.py
@@ -0,0 +1,7 @@
+# import modules
+from wallpaper import set_wallpaper, get_wallpaper
+# get current wallpaper's path
+print(get_wallpaper())
+ 
+# set your photo
+set_wallpaper("honkedbyclownsec.png")
