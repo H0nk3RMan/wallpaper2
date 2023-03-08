61da168901b5ebbba4685eb89f93d9a405c1de05
H0nk3RMan <127336752+H0nk3RMan@users.noreply.github.com>
2023-03-08 14:33:57 -0500

    Delete wall2.py

--- wall2.py
+++
@@ -1,7 +0,0 @@
-# import modules
-from wallpaper import set_wallpaper, get_wallpaper
-# get current wallpaper's path
-print(get_wallpaper())
- 
-# set your photo
-set_wallpaper("honkedbyclownsec.png")
